6b2c4ad7fd8e6f1850848455c22373c051cf1036
osama munawar <omunawar@cps.edu>
2014-03-26 20:13:45 +0000

    first commit

---
+++ RubyExam/bull.rb
@@ -0,0 +1,6 @@
+puts "do you like the bulls?"
+name = gets.chomp
+if (answer == 'yes' || answer == 'no')
+  break
+else
+  
---
+++ RubyExam/program2.rb
@@ -0,0 +1,5 @@
+classes = ['English' , 'french' , 'spanish']
+classes.each do |class|
+  puts 'i love ' + class + '!'
+  puts 'don\'t you?'
+  end
